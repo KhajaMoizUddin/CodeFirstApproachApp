024ba6b653c11638600e3a5215209573e9bbc0a1
KhajaMoizUddin <40728759+KhajaMoizUddin@users.noreply.github.com>
2020-05-15 02:48:00 +0530

    codeFirstApproach

---
+++ Migrations/202005061744191_CreateEmployeeTable.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeFirstMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateEmployeeTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Location = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
---
+++ Models/EmployeeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstMVC.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext() : base("EmployeeConnection")
+        {
+
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
